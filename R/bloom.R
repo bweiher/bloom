9f1f40bbeeac1b9f60afe4c8ccc91571e40a1529
ben weiher <bweiher@pm.me>
2018-04-22 13:58:49 -0700

    st

--- R/bloom.R
+++ R/bloom.R
@@ -12,37 +12,42 @@
 
 bloom <- function(datatable, cols, retain_all=TRUE, fn) {
 
-  #if(missing(...)) stop("You need to supply arguments to aggregate by")
+  # if(missing(...)) stop("You need to supply arguments to aggregate by")
 
   dlist <- list() # allocate list
   data.table::setDT(datatable) # ensure is data.table
-  grid <- branch(cols = cols, retain_all = retain_all)  # generate a dt of all possible input combinations
-  chr <- paste0(names(grid), ' == ')   # prepare filtering statements to apply
+  grid <- branch(cols = cols, retain_all = retain_all) # generate a dt of all possible input combinations
+  chr <- paste0(names(grid), " == ") # prepare filtering statements to apply
 
   # for each row in the grid ...
-  for(z in seq_along(1:nrow(grid))){
-    chr2 <- character(length=length(chr)) # allocate character vector
-    for(g in seq_along(chr2)) {  # create filter conditions for this row in the grid
-      if(grid[[g]][z] == 'All' || is.na(grid[[g]][z])) chr2[[g]] <- ''
-      else chr2[[g]] <- paste0(chr[g], "'", grid[[g]][z], "'")
+  for (z in seq_along(1:nrow(grid))) {
+    chr2 <- character(length = length(chr)) # allocate character vector
+    for (g in seq_along(chr2)) { # create filter conditions for this row in the grid
+      if (grid[[g]][z] == "All" || is.na(grid[[g]][z])) {
+        chr2[[g]] <- ""
+      } else {
+        chr2[[g]] <- paste0(chr[g], "'", grid[[g]][z], "'")
+      }
     }
 
     # filter out if setting == `All`
     chr2 <- chr2[nchar(chr2) > 0]
 
     # filter the datatable, or don't if there isn't a condition
-    filter_condition <- paste(chr2, collapse = ' & ')
-    if(filter_condition=='') agg_dt <- fn(datatable[,])
-    else agg_dt <- fn(datatable[eval(parse(text=filter_condition)),])
+    filter_condition <- paste(chr2, collapse = " & ")
+    if (filter_condition == "") {
+      agg_dt <- fn(datatable[, ])
+    } else {
+      agg_dt <- fn(datatable[eval(parse(text = filter_condition)), ])
+    }
 
 
     # for each column supplied, create a new one so that you know how the aggregation was done
-    for(g in seq_along(1:length(chr))){
-      agg_dt[, eval(parse(text="names(grid)[g] := grid[[g]][z]"))]
+    for (g in seq_along(1:length(chr))) {
+      agg_dt[, eval(parse(text = "names(grid)[g] := grid[[g]][z]"))]
     }
 
     dlist[[z]] <- agg_dt
-
   }
 
   data.table::rbindlist(dlist) # collapse list to dt
--- R/branch.R
+++ R/branch.R
@@ -6,16 +6,15 @@
 #' @param retain_all hold back an `All` option for each col supplied in cols
 
 branch <- function(cols, retain_all) {
-  
-  if(!is.data.table(cols)) stop("cols needs to be a data.table")
-  
-  
-  if(isTRUE(retain_all)) { # if we want to do it, then...
-    all_chosen <- setNames(data.table::data.table(matrix(nrow=1, ncol=ncol(cols))),names(cols)) # create dt
-    for (j in seq_along(names(cols))) data.table::set(all_chosen, j=j, value='All') # fill in All for values
+  if (!is.data.table(cols)) stop("cols needs to be a data.table")
+
+
+  if (isTRUE(retain_all)) { # if we want to do it, then...
+    all_chosen <- setNames(data.table::data.table(matrix(nrow = 1, ncol = ncol(cols))), names(cols)) # create dt
+    for (j in seq_along(names(cols))) data.table::set(all_chosen, j = j, value = "All") # fill in All for values
     cols <- data.table::rbindlist(list(all_chosen, cols)) # bind rows
-  } 
-  
+  }
+
   # generate a dataframe of all possible input combinations
-  unique(data.table::setDT(expand.grid(unique(cols),stringsAsFactors = FALSE)))
+  unique(data.table::setDT(expand.grid(unique(cols), stringsAsFactors = FALSE)))
 }
